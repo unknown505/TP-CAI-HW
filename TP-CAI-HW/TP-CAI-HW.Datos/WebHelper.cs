73eefcb386b9f3e203d45985a4a54c08a8cdedfa
Mariano Beraza <mariano.beraza@hotmail.com.ar>
2022-06-11 19:51:01 -0300

    Creación de capas
    Creación de capas

---
+++ TP-CAI-HW/TP-CAI-HW.Datos/WebHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_CAI_HW.Datos
+{
+    public class Class1
+    {
+    }
+}
