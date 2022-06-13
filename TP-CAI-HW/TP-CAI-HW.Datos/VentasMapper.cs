71dae1fcbd267e173271102b17d4144844c87d29
unknown505 <pablo.lin.97@gmail.com>
2022-06-13 05:28:54 -0300

    Ventas Y proveedores
    Creacion de obtencion de datos y funciones de proveedores y ventas

--- TP-CAI-HW/TP-CAI-HW.Datos/VentasMapper.cs
+++ TP-CAI-HW/TP-CAI-HW.Datos/VentasMapper.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TP_CAI_HW.Entidades;
+using TP_CAI_HW.Entidades.Dominio;
+using Newtonsoft.Json;
 namespace TP_CAI_HW.Datos
 {
-    class VentasMapper
+    public class VentasMapper
     {
+        private NameValueCollection ReverseMap(Ventas venta)
+        {
+            NameValueCollection n = new NameValueCollection();
+            n.Add("IdCliente", venta.IdCliente.ToString());
+            n.Add("IdProducto", venta.IdProducto.ToString());
+            n.Add("Cantidad", venta.Cantidad.ToString());
+            n.Add("Estad", venta.Estado.ToString());
+            n.Add("FechaAlta", venta.FechaAlta.ToString());
+            n.Add("FechaAlta", venta.FechaAlta.ToString());
+            n.Add("Usuario", venta.Usuario.ToString());
+            n.Add("IdVenta", venta.IdVenta.ToString());
+
+
+            return n;
+        }
+
+        public Ventas GetSellXId(int IdVenta)
+        {
+            string id = IdVenta.ToString();
+            string json = WebHelper.Get("VentaHardware/Ventas" + id);
+            Ventas p = JsonConvert.DeserializeObject<Ventas>(json);
+            if (p.IdProducto == IdVenta)
+            {
+                return p;
+            }
+            else
+            {
+                return p;
+                //throw Exception("error"); //Exception;
+            }
+
+        }
+        public List<Ventas> GetSell()
+        {
+            string json = WebHelper.Get("VentaHardware/Ventas");
+            List<Ventas> venta = JsonConvert.DeserializeObject<List<Ventas>>(json);
+            return venta;
+        }
+
+        public TransactionResult InsertSell(Ventas p)
+        {
+            NameValueCollection obj = ReverseMap(p);
+            string json = WebHelper.Post("VentaHardware/Ventas", obj);
+            TransactionResult rt = JsonConvert.DeserializeObject<TransactionResult>(json);
+            return rt;
+        }
     }
 }
--- TP-CAI-HW/TP-CAI-HW.Entidades/Dominio/Proveedores.cs
+++ TP-CAI-HW/TP-CAI-HW.Entidades/Dominio/Proveedores.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Configuration;
 
 namespace TP_CAI_HW.Entidades
 {
-    class Proveedores
+    public class Proveedores
     {
-        private string _idProveedor;
-        private string _idProducto;
+        private int _idProducto;
+        private string _nombre;
+        private string _apellido;
+        private string _email;
+        private string _cuit;
+        private bool _actividad;
         private DateTime _fechaAlta;
         private DateTime _fechaBaja;
+        private int _usuario;
+        private string _idProveedor;
+
         //private string _nombre;
 
-        public string IdProveedor { get => _idProveedor; set => _idProveedor = value; }
-        public string IdProducto { get => _idProducto; set => _idProducto = value; }
+        public int IdProducto { get => _idProducto; set => _idProducto = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Cuit { get => _cuit; set => _cuit = value; }
+        public bool Activo { get => _actividad; set => _actividad = value; }
         public DateTime FechaAlta { get => _fechaAlta; set => _fechaAlta = value; }
         public DateTime FechaBaja { get => _fechaBaja; set => _fechaBaja = value; }
+        public int Usuario { get => _usuario; set => _usuario = value; }
+        public string IdProveedor { get => _idProveedor; set => _idProveedor = value; }
+
+
 
         public Proveedores(int IdProveedor, int IdProducto)
         {
